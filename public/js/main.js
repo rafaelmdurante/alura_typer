3d0c8c388df3d52ef6a5bf6271f2cfd231d80c31
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2019-01-10 10:29:30 +0100

    Counter accuracy correction
    Implemented a RegExp to count the sentence more accurately. It was counting blank spaces as characteres before. Now that bug is corrected.

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,10 @@ tamanhoFrase.text(numPalavras);
 var campo = $(".campo-digitacao");
 campo.on("input", function(){
   var conteudo = campo.val();
-  var qtdCaracteres = conteudo.length;
+  // Retirar espaços da String
+  var conteudoSemEspaco = conteudo.replace(/\s+/g, '');
+  var qtdCaracteres = conteudoSemEspaco.length;
+  // Conta somente palavras separadas por espaço
   var qtdPalavras = conteudo.split(/\S+/).length - 1; // Substituição de " " por RegExp /\S+/ [...] - 1 para ficar mais correta contagem
   $("#contador-palavras").text(qtdPalavras);
   $("#contador-caracteres").text(qtdCaracteres);
