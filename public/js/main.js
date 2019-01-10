3b3095448e392d974e1ba1238d9834492e2a8e4e
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2019-01-10 10:13:55 +0100

    Input Counting Words and Characteres
    Typo correction in HTML textearea class and implementation of word counter

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,11 @@ var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numPalavras);
 
-console.log(numPalavras);
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+  var conteudo = campo.val();
+  var qtdCaracteres = conteudo.length;
+  var qtdPalavras = conteudo.split(/\S+/).length - 1; // Substituição de " " por RegExp /\S+/ [...] - 1 para ficar mais correta contagem
+  $("#contador-palavras").text(qtdPalavras);
+  $("#contador-caracteres").text(qtdCaracteres);
+});
