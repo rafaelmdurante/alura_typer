5505cf891bcac652318a315552464df8eec788fb
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2019-02-04 16:58:09 +0000

    Add Spinner
    - added a spinner gif to indicate the server is loading

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,12 +1,16 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
+  $("#spinner").show();
   $.get("http://localhost:3000/frases",trocaFraseAleatoria)
   .fail(function(){
     $("#erro").show();
     setTimeout(function(){
       $("#erro").hide();
     },2000);
+  })
+  .always(function(){
+    $("#spinner").hide();
   });
 }
 
